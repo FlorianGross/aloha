39c7aa568fafa42f86c55b4a9a6019c8702ead6d
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2021-05-07 16:29:22 +0200

    Notifications

--- lib/LocalNotifyManager.dart
+++ lib/LocalNotifyManager.dart
@@ -1,7 +1,8 @@
 import 'dart:io' show Platform;
 import 'package:rxdart/subjects.dart';
-import 'package:rxdart/subjects.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class LocalNotifyManager {
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -40,23 +41,73 @@ class LocalNotifyManager {
         android: androidInitilize, iOS: iOSinitilize);
     flutterLocalNotificationsPlugin.initialize(initializationsSettings);
   }
-  setOnNotificationRecieve(Function onNotificationRecieve){
+
+  setOnNotificationRecieve(Function onNotificationRecieve) {
     didRecieveLocalNotificationSubject.listen((notification) {
       onNotificationRecieve(notification);
     });
   }
-  setOnNotificationClick(Function onNotificationClick) async{
+
+  setOnNotificationClick(Function onNotificationClick) async {
     await flutterLocalNotificationsPlugin.initialize(initializationsSettings,
         onSelectNotification: (payload) async {
       onNotificationClick(payload);
     });
   }
 
-  Future<void> showNotification()async{
-    var androidChannel = AndroidNotificationDetails("CHANNEL_ID", "CHANNEL_NAME", "CHANNEL_DESCRIPTION", importance: Importance.max, priority: Priority.high);
+  Future<void> showNotification() async {
+    var androidChannel = AndroidNotificationDetails(
+        "CHANNEL_ID", "CHANNEL_NAME", "CHANNEL_DESCRIPTION",
+        importance: Importance.max,
+        priority: Priority.high,
+        icon: 'alcoly_logo');
     var iosChannel = IOSNotificationDetails();
-    var platformChannel = NotificationDetails(android: androidChannel, iOS: iosChannel);
-    await flutterLocalNotificationsPlugin.show(0, "TestTitle", "TestBody", platformChannel, payload: "New Payload");
+    var platformChannel =
+        NotificationDetails(android: androidChannel, iOS: iosChannel);
+    await flutterLocalNotificationsPlugin.show(
+        0, "TestTitle", "TestBody", platformChannel,
+        payload: "New Payload");
+  }
+
+  Future<void> scheduleNotificationNew(int day, int hour, int minute) async {
+    DateTime plannedDay = DateTime.now();
+    while (DateTime.now().weekday < day) {
+      plannedDay.add(Duration(days: 1));
+    }
+    tz.initializeTimeZones();
+    tz.TZDateTime dateTime = tz.TZDateTime(tz.local, plannedDay.year,
+        plannedDay.month, plannedDay.day, hour, minute);
+    var androidChannel = AndroidNotificationDetails(
+      "CHANNEL_ID",
+      "CHANNEL_NAME",
+      "CHANNEL_DESCRIPTION",
+      importance: Importance.max,
+      priority: Priority.high,
+      icon: 'alcoly_logo',
+    );
+    var iosChannel = IOSNotificationDetails();
+    var platformChannel =
+        NotificationDetails(android: androidChannel, iOS: iosChannel);
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      day,
+      "Getränke Scannen",
+      "Hast du heute vor etwas zu trinken? - Scannen nicht vergessen!",
+      dateTime,
+      platformChannel,
+      androidAllowWhileIdle: true,
+      matchDateTimeComponents: DateTimeComponents.dayOfWeekAndTime,
+      payload: "New Payload",
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
+
+  Future<void> cancelNotification(int id) async {
+    await flutterLocalNotificationsPlugin.cancel(id);
+  }
+
+  Future<void> cancelAllNotification() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
   }
 }
 
