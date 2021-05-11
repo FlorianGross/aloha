a2aac69820d6e50065109bcd695062c59224b4a7
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2021-05-11 18:58:21 +0200

    Rename of Label

--- lib/Pages/FirstStartPage.dart
+++ lib/Pages/FirstStartPage.dart
@@ -1,4 +1,4 @@
-import 'package:dieBruecke/Week.dart';
+import 'package:aloha/Week.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dieBruecke/main.dart';
+import 'package:aloha/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
