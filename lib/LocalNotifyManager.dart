f86935708ca7d8072335237093ba9f774a4b6d87
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2021-05-11 16:10:53 +0200

    Notifications

--- lib/LocalNotifyManager.dart
+++ lib/LocalNotifyManager.dart
@@ -1,7 +1,6 @@
 import 'dart:io' show Platform;
 import 'package:rxdart/subjects.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
 class LocalNotifyManager {
